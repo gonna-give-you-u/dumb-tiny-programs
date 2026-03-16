ae33acf023e94e0faabe0836d55b990029f224ee
very creative name <101212402+gonna-give-you-u@users.noreply.github.com>
2026-03-16 14:12:25 +0000

    tiny change
    also correct the comment about the 'and' mode

--- tagsearch.cpp
+++ tagsearch.cpp
@@ -3,10 +3,11 @@
 #include <vector>
 #include <sstream>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[]){ // 1 giant main function veri good programming 12/10
     int ao = 0; //arg_offset
     bool isExclude = false;
     bool fullLine = false;
@@ -26,6 +27,7 @@ int main(int argc, char* argv[]){
         << "\t-a (--and): find/exclude files that have all specified tags (default)" << endl // finally
         << "\t-o (--or): find/exclude files with any tag specified" << endl
         << "currently -x and other opts must be separate and in this order" << endl
+        << "also hint: if using a $() in bash, set `IFS=$'\\n';` before the 'for' to make spaces and special chars not break the $()" << endl
         ;
         return 1;
     }
@@ -64,7 +66,7 @@ int main(int argc, char* argv[]){
             return 1;
         }
     } else
-    if (string(argv[1+ao]) == "-a" || string(argv[1+ao]) == "--and"){ //currently doesn't work (you can use pipes and the stdin file "-" to emulate this)
+    if (string(argv[1+ao]) == "-a" || string(argv[1+ao]) == "--and"){ //works now (but you can use pipes and the stdin file "-" to emulate this)
         ao++;
         matchMode = 0;
         if (!argv[1+ao]){
@@ -88,81 +90,79 @@ int main(int argc, char* argv[]){
     }
 
     if(!*theInputStream){
-        cerr << "uh oh cant open the file: " << filename << endl;
+        cerr << "uh oh cant open the file: " << filename << endl
+		<< "is the file normal, does it exist, and do you have permission to read it" << endl
+        ;
         return 1;
     }
 
-    /*if (filename == "-"){ // stdin pipe
-        while (getline(cin,theText)){
-            if (theText.substr(0,2) != "##"){ // use ## to make a single-line comment in tagfiles
-                theLines.push_back(theText);
-            }
-        }
-    }*/ /*else{
-        cout << "open file" << endl;
-    }*/
-    vector<string> alreadyChecked;
-    int found;
-    int isPathChecked;
-    string currentLine;
-    string currentTagLine;
-    string currentPath;
-    string tagToPush;
+	{
+	    vector<string> alreadyChecked;
+	    int found;
+	    string currentLine;
+	    string currentTagLine;
+	    string currentPath;
+	    string tagToPush;
+	    int matchedTags = 0;
+	    bool shouldBreakOut = false;
 
-    while (getline(*theInputStream,currentLine)){
+	    while (getline(*theInputStream,currentLine)){
 
-        if (currentLine.substr(0,2) == "##") continue; //skip comments
-        currentPath = currentLine.substr(0,currentLine.find("::"));
-        currentTagLine = currentLine.substr(currentLine.find("::")+2);
-        stringstream tagsss(currentTagLine);
-        vector<string> currentTags;
-        while (getline(tagsss,tagToPush,',')){
-            currentTags.push_back(tagToPush);
-        }
+	        if (currentLine.substr(0,2) == "##") continue; //skip comments
+	        currentPath = currentLine.substr(0,currentLine.find("::"));
+	        currentTagLine = currentLine.substr(currentLine.find("::")+2);
+	        stringstream tagsss(currentTagLine);
+	        vector<string> currentTags;
+	        while (getline(tagsss,tagToPush,',')){
+	            currentTags.push_back(tagToPush);
+	        }
 
-        int matchedTags = 0;
+	        matchedTags = 0;
 
-        bool shouldBreakOut = false;
-        for (int j=0;j<currentTags.size();j++){
-            if (shouldBreakOut) break;
-            for (string s : tagsToFind){ // basically for each tag it searches for it, in OR mode if nothing found it moves onto the next tag
-                found = count(currentTags.begin(), currentTags.end(), s);
-                if (matchMode == 1){ // matchmode is 'or'
-                    if ((found > 0 && !isExclude) || (found <= 0 && isExclude)){
-                        if (fullLine) cout << currentLine << endl;
-                        else cout << currentPath << endl;
-                        shouldBreakOut = true; // once 1 tag matches, break out of 2 loops to avoid needless continuing
-                        break;
-                    }
-                } else if (matchMode == 0){ // matchmode is 'and'
-                    if (!isExclude){
-                        if (found > 0){
-                            if (matchedTags >= tagsToFind.size()){
-                                if (fullLine) cout << currentLine << endl;
-                                else cout << currentPath << endl;
-                                shouldBreakOut = true;
-                                break;
-                            } else{
-                                matchedTags++;
-                                continue;
-                            }
-                        } else{
-                            shouldBreakOut = true;
-                            break;
-                        }
-                    } else{
-                        if (found > 0){
-                            matchedTags++;
-                        }
-                    }
-                }
-            }
-            if (matchMode == 0 && isExclude && matchedTags < tagsToFind.size()){
-                if (fullLine) cout << currentLine << endl;
-                else cout << currentPath << endl;
-            }
-        }
-    }
+	        shouldBreakOut = false;
+	        for (string funniString : currentTags){
+	            if (shouldBreakOut) break;
+	            for (string s : tagsToFind){ // basically for each tag it searches for it, in OR mode if nothing found it moves onto the next tag
+	                found = count(currentTags.begin(), currentTags.end(), s);
+	                
+	                if (matchMode == 1){ // matchmode is 'or'
+	                    if ((found > 0 && !isExclude) || (found <= 0 && isExclude)){
+	                        if (fullLine) cout << currentLine << endl;
+	                        else cout << currentPath << endl;
+	                        shouldBreakOut = true; // once 1 tag matches, break out of 2 loops to avoid needless continuing
+	                        break;
+	                    }
+	                }
+	                else if (matchMode == 0){ // matchmode is 'and' (but not 'exclude')
+	                    if (!isExclude){
+	                        if (found > 0){
+	                            if (matchedTags >= tagsToFind.size()){
+	                                if (fullLine) cout << currentLine << endl;
+	                                else cout << currentPath << endl;
+	                                shouldBreakOut = true;
+	                                break;
+	                            } else{
+	                                matchedTags++;
+	                                continue;
+	                            }
+	                        } else{
+	                            shouldBreakOut = true;
+	                            break;
+	                        }
+	                    } else{
+	                        if (found > 0){
+	                            matchedTags++;
+	                        }
+	                    }
+	                }
+	            }
+	            if (matchMode == 0 && isExclude && matchedTags < tagsToFind.size()){ // matchmode is 'and' and 'exclude'
+	                if (fullLine) cout << currentLine << endl;
+	                else cout << currentPath << endl;
+	            }
+	        }
+	    }
+	}
 
     if (theFile)
         theFile.close();
